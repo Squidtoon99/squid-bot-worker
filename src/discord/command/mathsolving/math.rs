146d3ac45d4d3c0a05852189227f321809d46b80
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-11-04 21:31:51 -0700

    Bump to rust edition 2021 and format

--- src/discord/command/mathsolving/math.rs
+++ src/discord/command/mathsolving/math.rs
@@ -2,7 +2,8 @@ extern crate meval;
 use crate::discord::prelude::*;
 
 pub(crate) async fn math(ctx: &CommandContext) -> CommandResult {
-    if let Some(CommandOptionValue::String(expression)) = &ctx.arguments().first().map(|o| &o.value) {
+    if let Some(CommandOptionValue::String(expression)) = &ctx.arguments().first().map(|o| &o.value)
+    {
         let resp = match meval::eval_str(expression) {
             Ok(result) => format!("= `{}`", result),
             Err(e) => format!("I cannot solve that expression\n{:?}", e),
--- src/discord/command/utility/afk.rs
+++ src/discord/command/utility/afk.rs
@@ -1,14 +1,19 @@
 use crate::discord::prelude::*;
 
 pub(crate) async fn setafk(ctx: &CommandContext) -> CommandResult {
-    let msg = if let Some(CommandOptionValue::String(msg)) = &ctx.arguments().first().map(|o| &o.value) {
-        msg.trim()
-    } else {
-        ""
-    };
+    let msg =
+        if let Some(CommandOptionValue::String(msg)) = &ctx.arguments().first().map(|o| &o.value) {
+            msg.trim()
+        } else {
+            ""
+        };
     let redis = ctx.redis();
     if msg != "" {
-        let key = format!("afk:{}:{}", ctx.user.clone().unwrap().id, ctx.guild_id.unwrap());
+        let key = format!(
+            "afk:{}:{}",
+            ctx.user.clone().unwrap().id,
+            ctx.guild_id.unwrap()
+        );
 
         redis.set(key.as_str(), msg).await?;
     }
